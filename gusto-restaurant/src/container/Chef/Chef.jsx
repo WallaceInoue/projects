389cab7a542cb89d9b7d39e4543df9784cd1e290
WallaceInoue <137880705+WallaceInoue@users.noreply.github.com>
2024-02-05 19:38:05 +0900

    ok

--- gusto-restaurant/src/container/Chef/Chef.jsx
+++ gusto-restaurant/src/container/Chef/Chef.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
-
+import { images } from '../../constants';
+import { SubHeading } from '../../components';
 import './Chef.css';
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className='app__bg app__wrapper section__padding'>
+    <div className='app__wrapper_img app__wrapper_img-reverse'>
+      <img src={images.chef} alt="" />
+    </div>
+
+    <div className='app__wrapper_info'>
+      <SubHeading title="Chef's Word"/>
+      <h1 className='headtext__cormorant'>What We Believe In</h1>
+      
+      <div className='app__chef-content'>
+        <div className='app__chef-content_quote'>
+          <img src={images.quote} alt="" />
+          <p className='p__opensans'>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis illo doloribus pariatur totam, 
+          </p>
+        </div>
+        <p className='p__opensans'> 
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. In reprehenderit praesentium hic amet doloremque dolores non pariatur deserunt illum nulla vitae, blanditiis earum eligendi. Ducimus voluptatum aperiam saepe ex doloremque.
+        </p>
+      </div>
+
+      <div className='app__chef-sign'>
+        <p>Kevin Lou</p>
+        <p className='p__opensans'> Chef & Founder</p>
+        <img src={images.sign} alt="" />
+      </div>
+    </div>
   </div>
 );
 
